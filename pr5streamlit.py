ee4f0aeed8a188d0acf673983e8862d9fc94183a
Flare <82433193+Flare-workspace@users.noreply.github.com>
2024-10-03 22:50:32 +0530

    Added Streamlit files

---
+++ pr5streamlit.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle as pkl
+
+with open('p5Model1.pkl','rb') as f1:
+    model = pkl.load(f1)
+
+st.title("Height Prediction App")
+
+height = st.text_input("Enter your Height")
+
+if st.button("Predict"):
+    if height:
+        height = float(height)
+        prediction = model.predict([[height]])
+        st.write(f"Predicted weight {prediction[0]}")
+    else:
+        st.write('Please Enter the valid Height')
+    
